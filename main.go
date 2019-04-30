02fdf986760da7e742f8ca2429d5def0fba9eb65
Rahul Kumar <rahul@thoughtnirvana.com>
2019-05-01 02:19:47 +0530

    Added handlers

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/rahulkmr/chitchat/data"
+	"net/http"
+	"time"
+)
+
+// GET /err?msg=
+// shows the error message page
+func err(writer http.ResponseWriter, request *http.Request) {
+	vals := request.URL.Query()
+	_, err := session(writer, request)
+	if err != nil {
+		generateHTML(writer, vals.Get("msg"), "layout", "public.navbar", "error")
+	} else {
+		generateHTML(writer, vals.Get("msg"), "layout", "private.navbar", "error")
+	}
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	threads, err := data.Threads()
+	if err != nil {
+		error_message(writer, request, "Cannot get threads")
+	} else {
+		_, err := session(writer, request)
+		if err != nil {
+			generateHTML(writer, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(writer, threads, "layout", "private.navbar", "index")
+		}
+	}
+}
+
+func main() {
+	p("ChitChat", version(), "started at", config.Address)
+
+	// handle static assets
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	//
+	// all route patterns matched here
+	// route handler functions defined in other files
+	//
+
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
+
+	// defined in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// defined in route_thread.go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	// starting up the server
+	server := &http.Server{
+		Addr:           config.Address,
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.ReadTimeout * int(time.Second)),
+		WriteTimeout:   time.Duration(config.WriteTimeout * int(time.Second)),
+		MaxHeaderBytes: 1 << 20,
+	}
+	server.ListenAndServe()
+}
